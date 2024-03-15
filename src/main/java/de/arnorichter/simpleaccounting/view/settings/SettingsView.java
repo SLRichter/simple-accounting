66e4ef8a1b0bd31bbbbb4dba5893b21dd53b1fef
SLRichter <74209756+SLRichter@users.noreply.github.com>
2024-03-15 11:55:21 +0100

    feat: Swtich Theme

--- src/main/java/de/arnorichter/simpleaccounting/view/settings/SettingsView.java
+++ src/main/java/de/arnorichter/simpleaccounting/view/settings/SettingsView.java
@@ -1,33 +1,48 @@
 package de.arnorichter.simpleaccounting.view.settings;
 
-import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.Uses;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.dom.ThemeList;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.Lumo;
 import de.arnorichter.simpleaccounting.view.MainLayout;
 import jakarta.annotation.security.RolesAllowed;
 
+/**
+ * Die Klasse SettingsView repräsentiert die Ansicht für Einstellungen.
+ */
 @PageTitle("Settings")
 @Route(value = "settings", layout = MainLayout.class)
 @RolesAllowed("ADMIN")
 @Uses(Icon.class)
 public class SettingsView extends VerticalLayout {
 
-	public SettingsView() {
+	private Button lightButton;
 
-		var themeToggle = new Checkbox("Dark theme");
-		themeToggle.addValueChangeListener(e -> {
-			setTheme(e.getValue());
-		});
-		add(themeToggle);
+	/**
+	 * Konstruktor für die SettingsView.
+	 */
+	public SettingsView() {
+		lightButton = new Button("Switch Theme", this::setThemeLight);
+		add(lightButton);
 	}
 
-	private void setTheme(boolean dark) {
-		var js = "document.documentElement.setAttribute('theme', $0)";
-
-		getElement().executeJs(js, dark ? Lumo.DARK : Lumo.LIGHT);
+	/**
+	 * Methode zum Umschalten des Themes.
+	 *
+	 * @param event Das ClickEvent für den Button.
+	 */
+	private void setThemeLight(ClickEvent<Button> event) {
+		ThemeList test = UI.getCurrent().getElement().getThemeList();
+		if (test.isEmpty()) {
+			test.add(Lumo.DARK);
+		} else {
+			test.remove(Lumo.DARK);
+		}
 	}
 }
